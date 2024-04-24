2375b5bbab0d2afc23b167d355d95c5e1bb1719a
Ethan Pinto <ethanpintoa@gmail.com>
2024-04-24 01:21:45 -0400

    Make missing secret key error message more specific

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,10 +5,10 @@ dotenv.config();
 const hostname = process.env.HOSTNAME ?? "127.0.0.1";
 const port = parseInt(process.env.PORT ?? "3000");
 const secretKey =
-  process.env.SECRET_KEY ??
+  process.env.SECRET_KEY ||
   // Pattern is known as IIFE (Immediately Invoked Function Expression)
   (() => {
-    console.error("SECRET_KEY is not defined");
+    console.error("SECRET_KEY is not defined in environment variables.");
     process.exit(1);
   })();
 
