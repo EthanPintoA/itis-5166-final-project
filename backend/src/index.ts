e737df3972c453811913f0b2046f818da4bbdd18
Ethan Pinto <ethanpintoa@gmail.com>
2024-04-23 20:25:46 -0400

    Move hostname to .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,14 @@ import { StatusCodes } from "http-status-codes";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+const hostname = process.env.HOSTNAME ?? "127.0.0.1";
+const port = parseInt(process.env.PORT ?? "3000");
 
 app.get("/", (_, res) => {
   res.status(StatusCodes.OK).send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://127.0.0.1:${port}`);
+  console.log(`Server is running at http://${hostname}:${port}`);
 });
