7bda6f6b6b180e43f1cf0f1dfe6d11a52e1b4dfc
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-01 14:24:37 -0400

    Add graceful shutdown

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { StatusCodes } from "http-status-codes";
 import { hostname, port } from "./config";
 import authRouter from "./auth";
 import protectedRouter from "./protected";
+import { pool } from "./database";
 
 const app = express();
 
@@ -19,6 +20,26 @@ app.get("/", (_, res) => {
   res.status(StatusCodes.OK).send("Hello World!");
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running at http://${hostname}:${port}`);
+});
+
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, () => {
+    console.log("Shutting down server...");
+
+    server.close(async () => {
+      console.log("Server has shut down.");
+
+      await pool.end();
+      console.log("Connection pool has closed.");
+
+      process.exit(0);
+    });
+
+    setTimeout(() => {
+      console.error("Forcing shut down.");
+      process.exit(1);
+    }, 5000);
+  });
 });
