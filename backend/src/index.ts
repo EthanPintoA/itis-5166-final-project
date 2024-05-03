82aeae55487060e12643156bc6700fbad882d033
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-03 02:04:45 -0400

    Override Express' default 404 and error handler

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import bodyParser from "body-parser";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import compression from "compression";
 import helmet from "helmet";
-import { StatusCodes } from "http-status-codes";
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
 import { hostname, port } from "./config";
 import { pool } from "./database";
@@ -18,6 +18,19 @@ app.use(bodyParser.json());
 app.use(compression({ threshold: 0 }));
 
 app.use("/api", router);
+
+// Override the default 404 or not found handler
+app.use((_req, res, _next) => {
+  res.status(StatusCodes.NOT_FOUND).json({ message: "Not Found" });
+});
+
+// Override the default error handler
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: ReasonPhrases.INTERNAL_SERVER_ERROR });
+});
 
 const server = app.listen(port, () => {
   console.log(`Server is running at http://${hostname}:${port}`);
