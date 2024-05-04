d692a2e175120bb3ff97ba74ec9b8c93887f74b0
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-03 21:30:30 -0400

    Replace AppBar with custom navbar

--- frontend/src/lib/components/navbar.svelte
+++ frontend/src/lib/components/navbar.svelte
@@ -1,23 +1,51 @@
 <script>
-	import { AppBar } from '@skeletonlabs/skeleton';
-
 	import { util } from '$lib';
 </script>
 
-<AppBar>
-	<svelte:fragment slot="lead">
-		<strong class="text-xl">Personal Budget</strong>
-		<a class="btn btn-sm variant-ghost-surface" href="/"> Home </a>
-		<a class="btn btn-sm variant-ghost-surface" href="/dashboard"> Dashboard </a>
-		<a class="btn btn-sm variant-ghost-surface" href="/budget"> Budgets </a>
-		<a class="btn btn-sm variant-ghost-surface" href="/expenses"> Expenses </a>
-	</svelte:fragment>
-	<svelte:fragment slot="trail">
-		{#if util.isLoggedIn()}
-			<a class="btn btn-sm variant-ghost-surface" href="/logout"> Logout </a>
-		{:else}
-			<a class="btn btn-sm variant-ghost-surface" href="/login"> Login </a>
-			<a class="btn btn-sm variant-ghost-surface" href="/signup"> Signup </a>
-		{/if}
-	</svelte:fragment>
-</AppBar>
+<nav class="flex items-center justify-between flex-wrap variant-filled-surface p-6">
+	<div class="flex items-center flex-shrink-0 text-white mr-10">
+		<strong class="font-semibold text-xl tracking-tight">Personal Budget</strong>
+	</div>
+	<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+		<div class="text-sm lg:flex-grow">
+			<a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200 mr-6">
+				Home
+			</a>
+			<a
+				href="/dashboard"
+				class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200 mr-6"
+			>
+				Dashboard
+			</a>
+			<a
+				href="/budget"
+				class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200 mr-6"
+			>
+				Budget
+			</a>
+			<a href="/expenses" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200">
+				Expenses
+			</a>
+		</div>
+		<div>
+			{#if util.isLoggedIn()}
+				<a
+					href="/logout"
+					class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-5"
+					>Logout</a
+				>
+			{:else}
+				<a
+					href="/login"
+					class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-5"
+					>Login</a
+				>
+				<a
+					href="/signup"
+					class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+					>Signup</a
+				>
+			{/if}
+		</div>
+	</div>
+</nav>
