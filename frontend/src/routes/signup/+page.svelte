f6a167839b61ec4eb9eeba3a71875263d1f657fb
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-05 19:25:03 -0400

    Stop signing up twice

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -121,7 +121,6 @@
 				<button
 					type="submit"
 					class="flex w-full justify-center rounded-md bg-teal-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
-					on:click={handleSignIn}
 				>
 					Sign up
 				</button>
