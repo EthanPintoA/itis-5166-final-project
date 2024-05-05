c6166003a7e6e0d96bbb3521c36d1457426283f2
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-05 16:26:41 -0400

    Fix page headers

--- frontend/src/routes/budget/+page.svelte
+++ frontend/src/routes/budget/+page.svelte
@@ -148,8 +148,6 @@
 	let editingRow: number | null = null;
 </script>
 
-<h1 class="text-center h1">Budget</h1>
-
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 		<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -1,1 +1,1 @@
-<h1 class="text-center">Dashboard</h1>
+<h1 class="text-center h1 m-10">Dashboard</h1>
