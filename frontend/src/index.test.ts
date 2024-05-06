efdad1d87b68e0f7e686287196c34c8f2e9f7d3a
Ethan Pinto <ethanpintoa@gmail.com>
2024-05-05 21:54:58 -0400

    Add unit tests

--- frontend/src/index.test.ts
+++ frontend/src/index.test.ts
@@ -1,7 +1,55 @@
 import { describe, it, expect } from 'vitest';
+import jwt from 'jsonwebtoken';
 
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
+import { util } from '$lib';
+
+const mockLocalStorage = (() => {
+	let store = {} as Storage;
+
+	return {
+		getItem(key: string) {
+			return store[key];
+		},
+
+		setItem(key: string, value: string) {
+			store[key] = value;
+		},
+
+		removeItem(key: string) {
+			delete store[key];
+		},
+
+		clear() {
+			store = {} as Storage;
+		}
+	};
+})();
+
+Object.defineProperty(global, 'localStorage', { value: mockLocalStorage });
+
+describe('getTokenExpiration test', () => {
+	it('should return null if token is null', () => {
+		expect(util.getTokenExpiration()).toBe(null);
+	});
+
+	it('should return null if token is invalid', () => {
+		localStorage.setItem('token', 'invalid');
+		expect(util.getTokenExpiration()).toBe(null);
+	});
+
+	it('should return expiration time if token is valid', () => {
+		const token = jwt.sign({}, 'fake-secret-key', { expiresIn: '1m' });
+
+		localStorage.setItem('token', token);
+
+		expect(util.getToken()).toBe(token);
+
+		const decoded = jwt.decode(token);
+
+		expect(decoded).not.toBe(null);
+
+		const { exp } = decoded as { exp: number };
+
+		expect(util.getTokenExpiration()).toBe(exp * 1000);
 	});
 });
